7cc1f2bf7aacc19954637daf9822421694b50f02
Oskar Allerslev <oskar.m1660@gmail.com>
2026-03-01 22:22:41 +0100

    shuttle måske deploy api i fremtiden

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,22 @@
 
 
-use axum:: {
-    routing::get, 
-    Router,
-};
+use axum::{routing::get, Router};
 mod handlers;
 
 
 
 // and this is the localhost interface
-#[tokio::main]
-async fn main() {
+// #[tokio::main]
+#[shuttle_runtime::main]
+async fn main() -> shuttle_axum::ShuttleAxum {
     // this should  show our alive check funtion in the api
     let app = Router::new()
         .route("/is_alive", get(handlers::alive_check))
         .route("/data/{ticker}", get(handlers::get_ticker_data));
     
-    let adresse = "0.0.0.0:3000";
-    let listener = tokio::net::TcpListener::bind(adresse).await.unwrap();
+    // let adresse = "0.0.0.0:3000";
+    // let listener = tokio::net::TcpListener::bind(adresse).await.unwrap();
     
 
-    axum::serve(listener,app).await.unwrap();
+    Ok(app.into())
 }
